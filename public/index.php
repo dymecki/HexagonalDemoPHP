a76a14c1fac0fb8247f3616be65e5a4b38d30692
Michał Dymecki <michal@dymecki.com>
2017-07-26 17:39:40 +0200

    Add User creation

--- public/index.php
+++ public/index.php
@@ -3,3 +3,13 @@
 include_once 'InitLogger.php';
 
 echo 'HexagonalDemo 1.0';
+
+use Dymecki\HexagonalDemo\Domain\Model\User\User;
+
+$user = User::register(
+    \Ramsey\Uuid\Uuid::uuid4(),
+    'Michał',
+    'michal@dymecki.com'
+);
+
+var_dump((string) $user);
