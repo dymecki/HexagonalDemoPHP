5a132510f90dd21c8cb539169c55bd65f96e4e11
Michał Dymecki <michal@dymecki.com>
2017-07-26 17:19:53 +0200

    Implement UUID

--- src/Domain/Model/User/UserId.php
+++ src/Domain/Model/User/UserId.php
@@ -5,23 +5,29 @@ declare(strict_types = 1);
 namespace Dymecki\HexagonalDemo\Domain\Model\User;
 
 use Dymecki\HexagonalDemo\Domain\ValueObject;
+use Ramsey\Uuid\Uuid;
 
 final class UserId extends ValueObject
 {
     private $id;
 
-    public function __construct(string $id)
+    public function __construct(Uuid $id)
     {
         $this->id = $id;
     }
 
-    public function id(): string
+    public function id(): Uuid
     {
         return $this->id;
     }
 
+    public static function fromString(string $userId): self
+    {
+        return new self(Uuid::fromString($userId));
+    }
+
     public function __toString(): string
     {
-        return $this->id();
+        return $this->id()->toString();
     }
 }
