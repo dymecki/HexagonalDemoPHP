8410c0fa6643c776bbc1461eaa6cee26ec263e0f
Michał Dymecki <michal@dymecki.com>
2017-07-26 17:37:17 +0200

    Move logger init to the helper file

--- public/index.php
+++ public/index.php
@@ -1,24 +1,5 @@
 <?php
 
-    echo 'HexagonalDemo 1.0';
+include_once 'InitLogger.php';
 
-    $loader = require '../vendor/autoload.php';
-    //$loader->add('/Offer', __DIR__ . '/../src');
-
-    use Monolog\ErrorHandler;
-    use Monolog\Formatter\LineFormatter;
-    use Monolog\Handler\StreamHandler;
-    use Monolog\Logger;
-
-    $logger  = new Logger('Ads');
-    $handler = new StreamHandler(__DIR__ . '/../log/php_errors.log', Logger::DEBUG);
-
-    // Register the logger to handle PHP errors and exceptions
-    ErrorHandler::register($logger);
-
-    $lineFormatter = new LineFormatter;
-    $lineFormatter->includeStacktraces();
-    $handler->setFormatter($lineFormatter);
-
-    // Add log file handler
-    $logger->pushHandler($handler);
+echo 'HexagonalDemo 1.0';
