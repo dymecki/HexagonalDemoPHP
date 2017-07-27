4898da6059a62c7ac3fe1adbaca73264a4ac7a2d
Michał Dymecki <michal@dymecki.com>
2017-07-27 16:47:00 +0200

    Add new file

---
+++ src/Domain/Model/Car/FuelAmount.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Dymecki\HexagonalDemo\Domain\Model\Car;
+
+final class FuelAmount
+{
+    private $amount;
+    private $unit;
+
+    public function __construct(float $amount, string $unit)
+    {
+        if ($amount <= 0) {
+            throw new \DomainException('Fuel amount must be greater than zero: ' . $amount);
+        }
+
+        if ($unit == '') {
+            throw new \DomainException('Unit cannot be empty');
+        }
+
+        $this->amount = $amount;
+        $this->unit   = $unit;
+    }
+
+    public function amount(): float
+    {
+        return $this->amount;
+    }
+
+    public function unit(): string
+    {
+        return $this->unit;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf(
+            '%s %s',
+            $this->amount(),
+            $this->unit()
+        );
+    }
+}
