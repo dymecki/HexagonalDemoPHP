32e3fa4e2dd05f2b69dccfdc6e26ff4b1bbc024a
Michał Dymecki <michal@dymecki.com>
2017-07-27 16:34:28 +0200

    Implement AggregateRootInterface, add three empty properties

--- src/Domain/Model/Car/Car.php
+++ src/Domain/Model/Car/Car.php
@@ -8,5 +8,22 @@ use Dymecki\HexagonalDemo\Domain\AggregateRootInterface;
 
 final class Car implements AggregateRootInterface
 {
+    private $manufacturer;
+    private $model;
+    private $fuelAmount;
 
+    public function id()
+    {
+        // TODO: Implement id() method.
+    }
+
+    public function record($event)
+    {
+        // TODO: Implement record() method.
+    }
+
+    public function release()
+    {
+        // TODO: Implement release() method.
+    }
 }
