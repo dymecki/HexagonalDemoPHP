bae62744aa3eadba270c500cc92353a36d2a7615
Michał Dymecki <michal@dymecki.com>
2017-07-27 16:29:57 +0200

    Remove overwritten id() method

--- src/Domain/AggregateRootInterface.php
+++ src/Domain/AggregateRootInterface.php
@@ -6,8 +6,6 @@ namespace Dymecki\HexagonalDemo\Domain;
 
 interface AggregateRootInterface extends EntityInterface
 {
-    public function id();
-
     public function record($event);
 
     public function release();
