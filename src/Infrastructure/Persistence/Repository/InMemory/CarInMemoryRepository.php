b91a17ee975460f1e04114c4fb5f35eaf8e9a181
Michał Dymecki <michal@dymecki.com>
2017-07-27 17:54:54 +0200

    Add Car Repository files

---
+++ src/Infrastructure/Persistence/Repository/InMemory/CarInMemoryRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Dymecki\HexagonalDemo\Infrastructure\Persistence\Repository\InMemory;
+
+use Dymecki\HexagonalDemo\Domain\Model\Car\Car;
+use Dymecki\HexagonalDemo\Domain\Model\Car\CarId;
+use Dymecki\HexagonalDemo\Domain\Model\Car\CarRepositoryInterface;
+
+final class CarInMemoryRepository implements CarRepositoryInterface
+{
+    private $repository = [];
+
+    public function save(Car $car)
+    {
+        $this->repository[(string) $car->id()] = $car;
+    }
+
+    public function findById(CarId $carId): Car
+    {
+        if (isset($this->repository[(string) $carId])) {
+            return $this->repository[(string) $carId];
+        }
+
+        throw new \Exception('No car in the repository for CarId: ' . $carId);
+    }
+
+    public function remove(Car $car): void
+    {
+        unset($this->repository[(string) $car->id()]);
+    }
+}
