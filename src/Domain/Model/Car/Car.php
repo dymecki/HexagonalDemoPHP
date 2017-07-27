ce99bb048b558f092027389e5de933ae879c0023
Michał Dymecki <michal@dymecki.com>
2017-07-27 17:02:07 +0200

    Add constructor and getters

--- src/Domain/Model/Car/Car.php
+++ src/Domain/Model/Car/Car.php
@@ -5,16 +5,51 @@ declare(strict_types=1);
 namespace Dymecki\HexagonalDemo\Domain\Model\Car;
 
 use Dymecki\HexagonalDemo\Domain\AggregateRootInterface;
+use Ramsey\Uuid\Uuid;
 
 final class Car implements AggregateRootInterface
 {
-    private $manufacturer;
+    private $id;
+    private $brand;
     private $model;
     private $fuelAmount;
 
-    public function id()
+    private function __construct(CarId $id, CarBrand $brand, CarModel $model, FuelAmount $fuelAmount)
     {
-        // TODO: Implement id() method.
+        $this->id         = $id;
+        $this->brand      = $brand;
+        $this->model      = $model;
+        $this->fuelAmount = $fuelAmount;
+    }
+
+    public static function register(string $carBrand, string $carModel, float $fuelAmount, string $fuelUnit): self
+    {
+        return new self(
+            new CarId(Uuid::uuid4()),
+            new CarBrand($carBrand),
+            new CarModel($carModel),
+            new FuelAmount($fuelAmount, $fuelUnit)
+        );
+    }
+
+    public function id(): CarId
+    {
+        return $this->id();
+    }
+
+    public function brand(): CarBrand
+    {
+        return $this->brand;
+    }
+
+    public function model(): CarModel
+    {
+        return $this->model;
+    }
+
+    public function fuelAmount(): FuelAmount
+    {
+        return $this->fuelAmount;
     }
 
     public function record($event)
@@ -25,5 +60,14 @@ final class Car implements AggregateRootInterface
     public function release()
     {
         // TODO: Implement release() method.
+    }
+
+    public function __toString(): string
+    {
+        return sprintf(
+            '%s %s',
+            $this->brand(),
+            $this->model()
+        );
     }
 }
