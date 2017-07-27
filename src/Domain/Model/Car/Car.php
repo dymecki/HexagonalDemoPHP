7b27776bd2f045bb5b3aeb155103f0287dd0750b
Michał Dymecki <michal@dymecki.com>
2017-07-27 16:28:44 +0200

    Add new file

---
+++ src/Domain/Model/Car/Car.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dymecki\HexagonalDemo\Domain\Model\Car;
+
+use Dymecki\HexagonalDemo\Domain\AggregateRootInterface;
+
+final class Car implements AggregateRootInterface
+{
+
+}
