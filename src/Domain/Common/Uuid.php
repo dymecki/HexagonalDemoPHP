4df70508f646ac9026838241e3d6b3390fbdb37e
Michał Dymecki <michal@dymecki.com>
2017-07-29 11:37:18 +0200

    Add new file

---
+++ src/Domain/Common/Uuid.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Dymecki\HexagonalDemo\Domain\Model\User;
+
+use Dymecki\HexagonalDemo\Domain\Common\ValueObject;
+use Ramsey\Uuid\Uuid as RamseyUuid;
+use Ramsey\Uuid\UuidInterface;
+
+final class Uuid extends ValueObject
+{
+    private $id;
+
+    public function __construct(UuidInterface $id)
+    {
+        $this->id = $id;
+    }
+
+    public function id(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public static function fromString(string $userId): self
+    {
+        return new self(RamseyUuid::fromString($userId));
+    }
+
+    public function __toString(): string
+    {
+        return $this->id()->toString();
+    }
+}
