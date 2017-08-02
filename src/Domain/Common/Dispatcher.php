bb3c78f00344c47ae3d73b19ede4c27a3aef5270
Michał Dymecki <michal@dymecki.com>
2017-08-02 21:01:58 +0200

    Add bind() and registered() methods

--- src/Domain/Common/Dispatcher.php
+++ src/Domain/Common/Dispatcher.php
@@ -6,5 +6,15 @@ namespace Dymecki\HexagonalDemo\Domain\Common;
 
 final class Dispatcher
 {
+    private $listeners = [];
 
+    public function bind(string $eventName, ListenerInterface $listener): void
+    {
+        $this->listeners[$eventName][] = $listener;
+    }
+
+    public function registered(string $eventName): array
+    {
+        return isset($this->listeners[$eventName]) ? $this->listeners[$eventName] : [];
+    }
 }
