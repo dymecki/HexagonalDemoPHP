c3be46ba7ef1b282c08af714f631ea3c4000ce7d
Michał Dymecki <michal@dymecki.com>
2017-08-03 12:25:07 +0200

    Add new event files

---
+++ src/Domain/Model/User/Event/UserUpdatedEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dymecki\HexagonalDemo\Domain\Model\User\Event;
+
+use Dymecki\HexagonalDemo\Domain\Common\Event;
+
+final class UserUpdatedEvent extends Event
+{
+}
