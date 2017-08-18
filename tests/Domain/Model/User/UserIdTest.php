dcb6fb9d292fcc4d891bd189c4ae7bb8d4fbf699
Michał Dymecki <michal@dymecki.com>
2017-08-18 16:48:53 +0200

    Add new test file

---
+++ tests/Domain/Model/User/UserIdTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Dymecki\HexagonalDemo\Tests\Domain\Model\User;
+
+use Dymecki\HexagonalDemo\Domain\Model\User\UserId;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+class UserIdTest extends TestCase
+{
+    public function testProperId()
+    {
+        $uuid   = Uuid::uuid4();
+        $userId = new UserId($uuid);
+
+        $this->assertEquals($uuid, $userId->id());
+    }
+
+    public function testUserIdToString()
+    {
+        $uuid   = Uuid::uuid4();
+        $userId = new UserId($uuid);
+
+        $this->assertEquals($uuid->toString(), (string) $userId);
+    }
+
+    public function testUserIdFromString()
+    {
+        $uuid   = Uuid::uuid4();
+        $userId = UserId::fromString($uuid->toString());
+
+        $this->assertEquals($uuid->toString(), $userId->id());
+    }
+}
