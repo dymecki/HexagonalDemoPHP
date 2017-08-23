6324a8cf4b7bfa24dd8f7fab6a210876b34c2b49
Michał Dymecki <michal@dymecki.com>
2017-08-23 08:02:33 +0200

    Add sell() method

--- src/Domain/Model/Service/CarSellingService.php
+++ src/Domain/Model/Service/CarSellingService.php
@@ -4,7 +4,17 @@ declare(strict_types = 1);
 
 namespace Domain\Model\Service;
 
+use Dymecki\HexagonalDemo\Domain\Model\Car\Car;
+use Dymecki\HexagonalDemo\Domain\Model\User\User;
+
 final class CarSellingService implements ServiceInterface
 {
+    public function sell(User $seller, User $buyer, Car $car, $amount): bool
+    {
+        if ($seller->id() == $buyer->id()) {
+            throw new \Exception('Seller and buyer cannot be the same person.');
+        }
 
+        return true;
+    }
 }
