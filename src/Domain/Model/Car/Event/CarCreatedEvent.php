73d392bd4499a6f45615022f266a8ace89a6bbb4
Michał Dymecki <michal@dymecki.com>
2017-08-23 11:58:39 +0200

    Add new event file

---
+++ src/Domain/Model/Car/Event/CarCreatedEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dymecki\HexagonalDemo\Domain\Model\Car\Event;
+
+use Dymecki\HexagonalDemo\Domain\Common\Event;
+
+final class CarCreatedEvent extends Event
+{
+}
