ff1bfbd0e40319d94dcdc2d8f74c8965ce4c996d
Michał Dymecki <michal@dymecki.com>
2018-02-07 17:53:35 +0100

    Add new file

---
+++ src/Domain/Common/StoredEvent.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Domain\Common;
+
+final class StoredEvent implements EventInterface
+{
+    private $eventId;
+    private $content;
+    private $appearedAt;
+    private $name;
+
+    public function __construct($name, \DateTime $appearedAt, $content)
+    {
+        $this->name       = $name;
+        $this->appearedAt = $appearedAt;
+        $this->content    = $content;
+    }
+
+    public function appearedAt(): \DateTime
+    {
+        return $this->appearedAt;
+    }
+
+    public function eventId()
+    {
+        return $this->eventId;
+    }
+
+    public function content()
+    {
+        return $this->content;
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+}
