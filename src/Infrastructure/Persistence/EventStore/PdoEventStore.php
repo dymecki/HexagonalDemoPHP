e644df9acfb8e0d30e01393afb4178591ae9253d
Michał Dymecki <michal@dymecki.com>
2018-02-10 16:05:49 +0100

    Fix INSERT query

--- src/Infrastructure/Persistence/EventStore/PdoEventStore.php
+++ src/Infrastructure/Persistence/EventStore/PdoEventStore.php
@@ -6,6 +6,7 @@ namespace App\Infrastructure\Persistence\EventStore;
 
 use App\Domain\Event\Event;
 use App\Infrastructure\Service\Serializer;
+use Ramsey\Uuid\Uuid;
 
 final class PdoEventStore
 {
@@ -14,7 +15,7 @@ final class PdoEventStore
     public function __construct()
     {
         try {
-            $this->db = new \PDO('pgsql:host=localhost;port=54320;dbname=HexagonalDemo;user=homestead;password=secret');
+            $this->db = new \PDO('pgsql:host=localhost;port=5432;dbname=HexagonalDemo;user=homestead;password=secret');
         }
         catch (\PDOException $e) {
             echo $e->getMessage();
@@ -23,12 +24,19 @@ final class PdoEventStore
 
     public function add(Event $event)
     {
-        $aggregateType = 'BookTitleWasUpdated';
-        $aggregateId   = md5();
+        $aggregateType = rtrim(array_reverse(explode('\\', get_class($event)))[0], 'Event');
+        $aggregateId   = '7e622e4f-8b6e-4e26-bd34-1735d1c1d0e2';
         $version       = 1;
         $data          = Serializer::serialize($event);
 
-        $stmt = $this->db->prepare('INSERT INTO "EventStore" (AggregateType, AggregateId, version, data) VALUES (:aggregateType, :aggregateId, :version, :data)');
+//        var_dump($event);
+//        exit;
+
+        $stmt = $this->db->prepare('
+            INSERT INTO "EventStore" ("AggregateType", "AggregateId", version, data) 
+            VALUES (:aggregateType, :aggregateId, :version, :data)
+        ');
+
         $stmt->bindParam(':aggregateType', $aggregateType);
         $stmt->bindParam(':aggregateId', $aggregateId);
         $stmt->bindParam(':version', $version);
