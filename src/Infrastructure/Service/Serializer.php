8302c14cf8e0946ec044740affc1480a0f9490d1
Michał Dymecki <michal@dymecki.com>
2018-02-10 16:01:39 +0100

    Fix camel case serialization

--- src/Infrastructure/Service/Serializer.php
+++ src/Infrastructure/Service/Serializer.php
@@ -5,12 +5,16 @@ declare(strict_types = 1);
 namespace App\Infrastructure\Service;
 
 use JMS\Serializer\SerializerBuilder;
+use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
 
 final class Serializer
 {
     public static function serialize($data): string
     {
-        return SerializerBuilder::create()->build()->serialize($data, 'json');
+        return SerializerBuilder::create()
+                                ->setPropertyNamingStrategy(new IdenticalPropertyNamingStrategy())
+                                ->build()
+                                ->serialize($data, 'json');
     }
 
     public static function unserialize()
