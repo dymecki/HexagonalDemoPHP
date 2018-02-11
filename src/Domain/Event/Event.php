5a1fde55ce41bebc5bf1c86642132c1129e68988
Michał Dymecki <michal@dymecki.com>
2018-02-11 17:22:44 +0100

    Add __construct() and appearedAt() methods

--- src/Domain/Event/Event.php
+++ src/Domain/Event/Event.php
@@ -6,8 +6,20 @@ namespace App\Domain\Event;
 
 abstract class Event implements EventInterface
 {
+    protected $appearedAt;
+
+    public function __construct()
+    {
+        $this->appearedAt = new \DateTime();
+    }
+
     public function name(): string
     {
-        return (new \ReflectionClass($this))->getShortName();
+        return rtrim((new \ReflectionClass($this))->getShortName(), 'Event');
+    }
+
+    public function appearedAt(): \DateTime
+    {
+        return $this->appearedAt;
     }
 }
